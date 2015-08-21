ebef6cf958802e60c0845d96612d6cef6ad2a9b7
Jan Corazza <corazzajan@gmail.com>
2015-08-22 01:20:34 +0200

    Update for Windows dev

--- monac/src/main/scala/org/monalang/monac/MonacMain.scala
+++ monac/src/main/scala/org/monalang/monac/MonacMain.scala
@@ -1,6 +1,6 @@
 package org.monalang.monac
 
-import java.io.{BufferedReader, FileReader}
+import java.io.{InputStreamReader, BufferedReader, FileReader}
 
 import org.monalang.monac.common.util.Reader
 import org.monalang.monac.front._
@@ -8,12 +8,11 @@ import org.monalang.monac.iface.CompileOptions
 
 object MonacMain extends App {
   val configuration = CompileOptions(args.toList)
-  val lexer = new Lexer(new BufferedReader(
-    new FileReader("/home/jan/Projects/Mona/monac-run/hello.mona")))
+  val lexer = new Lexer(new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("/standard-library/Data/collection.mona"))))
 
   println(lexer.tokenStream.takeWhile(_ != EndOfSource).toList)
 
   val parser = new Parser()
 
-  println(Reader.readWhole(getClass().getResourceAsStream("/standard-library/runtime.mona")))
+  //println(Reader.readWhole(getClass().getResourceAsStream("/standard-library/Data/collection.mona")))
 }
